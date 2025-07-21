3f97566b70e3b4bcd2be6cff410e9f055c31feb8
ZTS-Mac <ztsthu@gmail.com>
2025-07-22 02:04:50 +0800

    feat: moved to yarn

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		strictPort: true,
+		fs: {
+			// Allow serving files from one level up to the project root
+			allow: ['..'],
+		},
 	},
 	build: {
 		target: ['chrome64', 'edge79', 'firefox62', 'safari11.1'],
